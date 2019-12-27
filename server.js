31eb1e172da3280d08d8a6c5602dbeba323dbe79
Andrei Sevostianov <andrei@superstruct.tech>
2019-12-27 10:46:59 -0800

    style: make code more readable

--- server.js
+++ server.js
@@ -1,28 +1,31 @@
-const http = require("http");
+const http = require('http')
 const _ = require('lodash')
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
 const server = http.createServer((req, res) => {
-  if (req.url === "/") return respondHello(req, res);
-  if (req.url === "/user-agent") return respondU(req, res);
-  if (req.url.match(/^\/b64\//)) return respondB(req, res);
+  if (req.url === '/') return respondHello(req, res)
+  if (req.url === '/user-agent') return respondUserAgent(req, res)
+  if (req.url.match(/^\/b64\//)) return respondBase64(req, res)
   if (req.url === '/repetitive-word') return respondRepetitiveWord(req, res)
 
-  res.end();
-});
+  res.end()
+})
 
-function respondHello(req, res) {
-  res.end(JSON.stringify({ msg: "hello" }));
+function respondHello (req, res) {
+  res.end(JSON.stringify({ msg: 'hello' }))
 }
 
-function respondU(req, res) {
-  const ua = req.headers["user-agent"];
-  res.end(JSON.stringify({ ua }));
+function respondUserAgent (req, res) {
+  const ua = req.headers['user-agent']
+  res.end(JSON.stringify({ ua }))
 }
 
-function respondB(req, res) {
-  res.end(JSON.stringify({b64: Buffer.from(req.url.replace(/^\/b64\//, "")).toString("base64")}));
+function respondBase64 (req, res) {
+  const base64 = req.url.replace(/^\/b64\//, '')
+  res.end(
+    JSON.stringify({
+      b64: Buffer.from(base64).toString('base64') }))
 }
 
 function respondRepetitiveWord (req, res) {
@@ -37,10 +40,10 @@ function respondRepetitiveWord (req, res) {
   })
 }
 
-server.listen(PORT);
-console.log(`Server listening on port ${PORT}`);
+server.listen(PORT)
+console.log(`Server listening on port ${PORT}`)
 
-if (require.main !== module) module.exports = server;
+if (require.main !== module) module.exports = server
 
 function countWords (text) {
   const parsedText = {}
